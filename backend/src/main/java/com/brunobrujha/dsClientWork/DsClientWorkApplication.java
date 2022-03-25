163d85394e5fb4950a6b929a5300fe1dc6661eac
BrunoBrujha <brunobrujha@gmail.com>
2022-03-25 10:36:05 -0300

    Criacao do projeto

---
+++ backend/src/main/java/com/brunobrujha/dsClientWork/DsClientWorkApplication.java
@@ -0,0 +1,13 @@
+package com.brunobrujha.dsClientWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientWorkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brunobrujha/dsClientWork/DsClientWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobrujha.dsClientWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
